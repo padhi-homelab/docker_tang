88d304537cab5705c5c6703b92d210536e7da19c
yehy4 <25834760+yehy4@users.noreply.github.com>
2022-08-11 23:22:37 +0200

    Fix exit codes in health check script (#51)
    * Fix exit codes in health check script
    
    * Use if statements in all shell scripts for consistency

--- check-servers.sh
+++ check-servers.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
-[ "$ENABLE_IPv4" = "1" ] && wget -4qSO /dev/null http://127.0.0.1:8080/adv
-[ "$ENABLE_IPv6" = "1" ] && wget -6qSO /dev/null http://[::1]:8080/adv
+if [ "$ENABLE_IPv4" = "1" ]; then
+  wget -4qSO /dev/null http://127.0.0.1:8080/adv
+fi
+
+if [ "$ENABLE_IPv6" = "1" ]; then
+  wget -6qSO /dev/null http://[::1]:8080/adv
+fi
--- start-servers.sh
+++ start-servers.sh
@@ -2,7 +2,12 @@
 
 TANG="system:'REMOTE_ADDR=\$SOCAT_PEERADDR tangd /db'"
 
-[ "$ENABLE_IPv4" = "1" ] && socat TCP4-LISTEN:8080,reuseaddr,fork "$TANG" &
-[ "$ENABLE_IPv6" = "1" ] && socat TCP6-LISTEN:8080,ipv6only=1,reuseaddr,fork "$TANG" &
+if [ "$ENABLE_IPv4" = "1" ]; then
+  socat TCP4-LISTEN:8080,reuseaddr,fork "$TANG" &
+fi
+
+if [ "$ENABLE_IPv6" = "1" ]; then
+  socat TCP6-LISTEN:8080,ipv6only=1,reuseaddr,fork "$TANG" &
+fi
 
 wait
